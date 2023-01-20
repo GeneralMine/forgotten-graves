c42483971556f6e736df38f0451e795199d3dde5
Ginsm <mattg.0609@gmail.com>
2023-01-20 18:27:01 -0500

    Fix two bugs; see desc
    Bug 1:
    When retrievalType is set to "DROP" your inventory should not drop
    as well.
    
    Bug 2:
    There was an edge case where armor could sometimes be duplicated. This
    has been fixed as well.

--- src/main/java/me/mgin/graves/block/api/RetrieveGrave.java
+++ src/main/java/me/mgin/graves/block/api/RetrieveGrave.java
@@ -36,22 +36,24 @@ public class RetrieveGrave {
 			if (!Permission.playerCanOverride(player))
 				return false;
 
-		// Store old inventories as one big inventory
-		DefaultedList<ItemStack> oldInventory = DefaultedList.of();
-
-		for (InventoriesApi api : Graves.inventories) {
-			DefaultedList<ItemStack> inventory = api.getInventory(player);
-
-			if (inventory == null)
-				continue;
-
-			oldInventory.addAll(inventory);
-		}
-
 		// Resolve drop type
 		GraveDropType dropType = GravesConfig.resolveConfig("dropType", player.getGameProfile()).main.dropType;
 
 		if (dropType == GraveDropType.PUT_IN_INVENTORY) {
+			// Store old inventories as one big inventory
+			DefaultedList<ItemStack> oldInventory = DefaultedList.of();
+
+			for (InventoriesApi api : Graves.inventories) {
+				DefaultedList<ItemStack> inventory = api.getInventory(player);
+
+				// Skip empty inventories
+				if (inventory == null)
+					continue;
+
+				oldInventory.addAll(inventory);
+				api.clearInventory(player);
+			}
+
 			DefaultedList<ItemStack> extraItems = DefaultedList.of();
 
 			// Equip inventories
